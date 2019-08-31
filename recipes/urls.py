f324ffecd19e3997cd32e038b607b23d73e8f405
lailalelouch <39729644+lailalelouch@users.noreply.github.com>
2019-08-31 20:16:07 +0300

    add my recipes url

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,5 +5,6 @@ from recipes import views
 urlpatterns = [
     path('', views.recipes_list, name="recipes-list"),
     path('recipe/<recipe_slug>/', views.recipe_details, name="recipe-details"),
+    path('my-recipes/', views.my_recipes_list, name="my-recipes-list"),
 ]
 
